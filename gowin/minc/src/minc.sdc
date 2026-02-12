03f4addd0d8b4bae156ac66f3d96ceef1bcda21e
nasu8151 <nasu.tougarashi@gmail.com>
2026-02-12 17:46:19 +0900

    BSRAM動作した

--- gowin/minc/src/minc.sdc
+++ gowin/minc/src/minc.sdc
@@ -2,6 +2,5 @@
 //All rights reserved.
 //File Title: Timing Constraints file
 //Tool Version: V1.9.11.03 Education 
-//Created Time: 2026-02-06 16:37:14
+//Created Time: 2026-02-06 17:08:49
 create_clock -name sys_clk -period 37.037 -waveform {0 18.518} [get_ports {sys_clk}]
-create_clock -name int_clk -period 1000 -waveform {0 500} [get_nets {int_clk}] -add
--- gowin/minc/src/minc_gw_top.sv
+++ gowin/minc/src/minc_gw_top.sv
@@ -1,32 +1,59 @@
 module minc_gw_top (
-    input  wire        sys_clk,
-    input  wire        sys_nrst,
-    output wire [7:0]  pc_out,
-    output wire [7:0]  top_out,
-    output wire [7:0]  sp_out
+    input  logic        sys_clk,
+    input  logic        sys_nrst,
+    output logic [7:0]  pc_out,
+    output logic [7:0]  top_out,
+    output logic [7:0]  address
 );
 
-    logic [23:0] presc_cnt;
-    logic        int_clk;
+//    logic [23:0] presc_cnt;
+//    logic        int_clk;
+
+    logic [7:0] sp_out;
+    logic [7:0] data_in;
+    logic [7:0] data_out;
+    logic       we;
 
     minc u_minc (
-        .CLK    (int_clk),
-        .nRESET (sys_nrst),
-        .pc_out (pc_out),
-        .top_out(top_out),
-        .sp_out (sp_out)
+        .clk    (sys_clk),
+        .reset_n (sys_nrst),
+        .pc_out(pc_out),
+        .sp_out(sp_out),
+        .address(address),
+        .data_out(data_out),
+        .we(we),
+        .data_in(data_in)
     );
 
-    always_ff @(posedge sys_clk or negedge sys_nrst) begin
-        if (!sys_nrst) begin
-            presc_cnt <= 24'd0;
-            int_clk   <= 1'b0;
-        end else if (presc_cnt == 24'd13_499) begin
-            presc_cnt <= 24'd0;
-            int_clk   <= ~int_clk;
-        end else begin
-            presc_cnt <= presc_cnt + 24'd1;
+    Gowin_SP ram(
+        .dout(data_in), //output [7:0] dout
+        .clk(~sys_clk), //input clk
+        .ce(1'b1), //input ce
+        .oce(1'b1), //input oce
+        .reset(~sys_nrst), //input reset
+        .wre(we), //input wre
+        .ad(address), //input [7:0] ad
+        .din(data_out) //input [7:0] din
+    );
+
+    always_ff @( posedge sys_clk ) begin
+        if (sys_nrst) begin
+            top_out <= data_in;
+        end else if (we) begin
+            top_out <= data_out;
         end
     end
+
+//    always_ff @(posedge sys_clk or negedge sys_nrst) begin
+//        if (!sys_nrst) begin
+//            presc_cnt <= 24'd0;
+//            int_clk   <= 1'b0;
+//        end else if (presc_cnt == 24'd13_499) begin
+//            presc_cnt <= 24'd0;
+//            int_clk   <= ~int_clk;
+//        end else begin
+//            presc_cnt <= presc_cnt + 24'd1;
+//        end
+//    end
 
 endmodule
